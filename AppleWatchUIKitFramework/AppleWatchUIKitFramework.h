81b22b99ed041d45bfa95c0750801cdabeb4c289
Spencer Elliott <me@elliottsj.com>
2016-05-28 17:01:56 -0400

    Create Watch Framework target "AppleWatchUIKitFramework"

---
+++ AppleWatchUIKitFramework/AppleWatchUIKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  AppleWatchUIKitApp.h
+//  AppleWatchUIKitApp
+//
+//  Created by Spencer Elliott on 2016-05-28.
+//  Copyright © 2016 Spencer Elliott. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for AppleWatchUIKitApp.
+FOUNDATION_EXPORT double AppleWatchUIKitAppVersionNumber;
+
+//! Project version string for AppleWatchUIKitApp.
+FOUNDATION_EXPORT const unsigned char AppleWatchUIKitAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppleWatchUIKitApp/PublicHeader.h>
+
+
