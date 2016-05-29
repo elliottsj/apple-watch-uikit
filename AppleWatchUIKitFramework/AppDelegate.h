1bdf0d0c9e714c745ae07f4447d71840543523c3
Spencer Elliott <me@elliottsj.com>
2016-05-29 14:56:57 -0400

    Create AppDelegate.h

---
+++ AppleWatchUIKitFramework/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppleWatchUIKit
+//
+//  Created by Spencer Elliott on 2016-05-29.
+//  Copyright © 2016 Spencer Elliott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
